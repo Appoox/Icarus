bbbb88ee863413710fbc00645077bbfbfeda840b
Appoox <gadha1998@gmail.com>
2026-02-11 10:40:22 +0530

    [fix] corrected the author model

--- articles/models.py
+++ articles/models.py
@@ -1,25 +1,13 @@
 from django.db import models
-from modelcluster.fields import ParentalManyToManyField
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 
-# Create your models here.
-
-class Article(Page):
-    author = ParentalManyToManyField('Author', blank = True)
-    date = models.DateField(("Date of publishing"), auto_now=False, auto_now_add=False)
-    body = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('author'),
-        FieldPanel('date'),
-        FieldPanel('body'),
-    ]
-
+# 1. THE AUTHOR PAGE
 class Author(Page):
-    role = models.CharField(("Title/Role"), max_length=50)
-    bio = models.TextField(("Author bio"))
+    role = models.CharField("Title/Role", max_length=50)
+    bio = models.TextField("Author bio")
     profile_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -28,16 +16,28 @@ class Author(Page):
         related_name='+'
     )
 
-
-    panels = [
+    content_panels = Page.content_panels + [
         FieldPanel('role'),
         FieldPanel('bio'),
         FieldPanel('profile_image'),
     ]
 
-    def __str__(self):
-        return self.title
+class ArticleAuthorRelationship(Orderable):
+    page = ParentalKey('Article', related_name='article_authors', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', related_name='author_articles', on_delete=models.CASCADE)
 
-    def get_articles(self):
-        """Get all articles by this author"""
-        return Article.objects.live().filter(author=self)
+    panels = [
+        FieldPanel('author'),
+    ]
+
+# 3. THE ARTICLE PAGE
+class Article(Page):
+    date = models.DateField("Date of publishing")
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        # We use InlinePanel to manage the relationship
+        InlinePanel('article_authors', label="Authors"),
+        FieldPanel('body'),
+    ]
