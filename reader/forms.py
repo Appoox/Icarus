bdbc23ec8f85948d903c6b437eed0249666ccee7
Appoox <gadha1998@gmail.com>
2026-03-01 13:33:05 +0530

    [feature] Reader app and payall set up

---
+++ reader/forms.py
@@ -0,0 +1,87 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Reader
+from issue.models import Topic
+
+
+class ReaderSignupForm(UserCreationForm):
+    """
+    Extends Django's UserCreationForm with the Reader-specific fields.
+    Creates both the User and the linked Reader profile.
+    """
+    name = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Full name',
+            'class': 'form-input',
+        }),
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Email address',
+            'class': 'form-input',
+        }),
+    )
+    phone_number = forms.CharField(
+        max_length=20,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Phone number',
+            'class': 'form-input',
+        }),
+    )
+    interested_topics = forms.ModelMultipleChoiceField(
+        queryset=Topic.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'topic-checkbox',
+        }),
+        help_text='Select topics you are interested in.',
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Username',
+                'class': 'form-input',
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Style the password fields
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Password',
+            'class': 'form-input',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm password',
+            'class': 'form-input',
+        })
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email is already registered.')
+        if Reader.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email is already registered.')
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            reader = Reader.objects.create(
+                user=user,
+                name=self.cleaned_data['name'],
+                email=self.cleaned_data['email'],
+                phone_number=self.cleaned_data.get('phone_number', ''),
+            )
+            topics = self.cleaned_data.get('interested_topics')
+            if topics:
+                reader.interested_topics.set(topics)
+        return user
---
+++ reader/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('signup/', views.reader_signup, name='reader_signup'),
+    path('profile/', views.reader_profile, name='reader_profile'),
+    path('logout/', views.reader_logout, name='reader_logout'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='reader/login.html'),
+        name='reader_login',
+    ),
+]
