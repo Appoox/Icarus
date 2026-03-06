33d97392445e3b4666a4677c1bb11eca5f838b3f
Appoox <gadha1998@gmail.com>
2026-03-06 15:54:56 +0530

    [feature] Implement topic detail pages, making topic categories clickable and displaying associated articles and issues.

--- issue/views.py
+++ issue/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Topic, Issue
+from articles.models import Article
 
-# Create your views here.
+def topic_detail(request, slug):
+    topic = get_object_or_404(Topic, slug=slug)
+    # Get articles and issues for this topic
+    articles = Article.objects.live().filter(topic=topic).order_by('-first_published_at')
+    issues = Issue.objects.live().filter(topic=topic).order_by('-date_of_publishing')
+    
+    return render(request, 'issue/topic_detail.html', {
+        'topic': topic,
+        'articles': articles,
+        'issues': issues,
+    })
