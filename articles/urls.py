c239fba5c4bee7eb1f1a70f45149a6ae58d6f7f8
Appoox <gadha1998@gmail.com>
2026-04-18 00:19:18 +0530

    [feature] implement tag detail pages and link tags to filter articles by category

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('track-read-fully/', views.track_read_fully, name='track_read_fully'),
+    path('tags/<str:tag>/', views.tag_detail, name='tag_detail'),
 ]
