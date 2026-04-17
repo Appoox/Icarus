443637f3c560bc466698fcfed521cf8f4bdebd10
Appoox <gadha1998@gmail.com>
2026-04-18 00:19:18 +0530

    [feature] implement tag detail pages and link tags to filter articles by category

--- articles/views.py
+++ articles/views.py
@@ -23,3 +23,18 @@ def track_read_fully(request):
         return JsonResponse({'status': 'ignored', 'message': 'Already counted in this session'})
     
     return JsonResponse({'status': 'ignored', 'message': 'Staff views not counted'})
+
+from django.shortcuts import render
+from taggit.models import Tag
+
+def tag_detail(request, tag):
+    tag_obj = Tag.objects.filter(name=tag).first()
+    if tag_obj:
+        articles = Article.objects.live().filter(tags__name=tag_obj.name).order_by('-first_published_at')
+    else:
+        articles = []
+    
+    return render(request, 'articles/tag_detail.html', {
+        'tag_name': tag_obj.name if tag_obj else tag,
+        'articles': articles,
+    })
