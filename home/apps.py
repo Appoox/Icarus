8a549b599258867abda6d201313593c37fa4c663
Appoox <gadha1998@gmail.com>
2026-04-27 12:55:00 +0530

    [feature] integrate django-auditlog to track model changes and add admin dashboard panels

--- home/apps.py
+++ home/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
+
+    def ready(self):
+        from auditlog.registry import auditlog
+        from .models import SiteHeader, SiteFooter, FooterLink, HomePage
+        from wagtail.images.models import Image
+        from wagtail.documents.models import Document
+        
+        auditlog.register(SiteHeader)
+        auditlog.register(SiteFooter)
+        auditlog.register(FooterLink)
+        auditlog.register(HomePage)
+        auditlog.register(Image)
+        auditlog.register(Document)
--- literati/apps.py
+++ literati/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class LiteratiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'literati'
+
+    def ready(self):
+        from auditlog.registry import auditlog
+        from .models import (
+            Literati, AuthorIndexPage, ArticleAuthorRelationship, 
+            EditorialBoard, EditorialBoardMember
+        )
+        auditlog.register(Literati)
+        auditlog.register(AuthorIndexPage)
+        auditlog.register(ArticleAuthorRelationship)
+        auditlog.register(EditorialBoard)
+        auditlog.register(EditorialBoardMember)
